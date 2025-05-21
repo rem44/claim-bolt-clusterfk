917bf00fd6584306745eac3e949c9c7dbe99e51d
rem44 <79606312+rem44@users.noreply.github.com>
2025-05-21 02:02:23 -0400

    Fix Build Error - Sample Data Directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    copyPublicDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      },
+    }
   },
   publicDir: 'public',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
