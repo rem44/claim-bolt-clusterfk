9a83ac5632974bf914ffe669000cb8b8fb52a4f9
rem44 <79606312+rem44@users.noreply.github.com>
2025-05-21 01:52:06 -0400

    Fix Build Error

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true,
+  },
+  publicDir: 'public',
 });
